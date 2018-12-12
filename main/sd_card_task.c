a915e0005c0a9bc87776a0afe884b7adfa5942bc
anti-cdq <anti910427@icloud.com>
2018-12-12 19:37:42 +0800

    BMP display bug fixed

--- main/sd_card_task.c
+++ main/sd_card_task.c
@@ -24,23 +24,34 @@
 
 #include "lcd.h"
 #include "global_config.h"
+#include "button.h"
 
 static const char *TAG = "SD CARD";
 
 
 #define FILE_PATH_LEN			50
-#define FILE_NUM				50
+#define FILE_NUM_PER_PAGE		10
 #define FILE_NAME_LEN			30
 #define FILE_BUFF_LEN			1024
 #define LCD_BUFF_LEN			(LCD_W*2)
 
-char *all_file_name;
-char *file_path;
-uint8_t *Buff;
-uint8_t *buff_to_lcd;
-static uint8_t display_flag = 0;
+typedef struct
+{
+	uint8_t display_flag;
+	uint8_t status;
+	uint32_t file_num;
+	uint32_t file_index;
+	uint8_t	Buff[LCD_W*3];
+	uint8_t buff_to_lcd[LCD_BUFF_LEN];
+	char file_path[FILE_PATH_LEN];
+	char all_file_name[FILE_NUM_PER_PAGE][FILE_NAME_LEN];
+}file_browser_s;
 
-void show_bmp_center(char* path)
+file_browser_s *file_browser;
+uint8_t *button_evt;
+
+
+void show_bmp_center(char* path, uint16_t offsetX, uint16_t offsetY)
 {
 	uint16_t x1=0, y1=0, x2=LCD_W, y2=LCD_H;		//图片显示区域
 	uint16_t offset_width = 0, offset_height = 0;
@@ -49,19 +60,20 @@ void show_bmp_center(char* path)
 	uint16_t x = 0, y = 0;
 	uint32_t temp = 0;
 
-	memset(buff_to_lcd, 0, LCD_BUFF_LEN);
+	memset(file_browser->buff_to_lcd, 0, LCD_BUFF_LEN);
 	LCD_Clear(BLACK);
 
 	FILE *f = fopen(path, "r");
 	if (f == NULL)
 	{
-		ESP_LOGE(TAG, "Failed to open file for reading");
+		ESP_LOGE(TAG, "1.Failed to open file for reading");
+		printf("%s\n", path);
 		return;
 	}
-	fgets((char *)Buff, 54, f);
+	fread((char *)file_browser->Buff, 54, 1, f);
 
-	image_width = (uint16_t)Buff[19]<<8 | (uint16_t)Buff[18];	//获取图片宽度信息
-	image_height = (uint16_t)Buff[23]<<8 | (uint16_t)Buff[22];	//获取图片高度信息
+	image_width = (uint16_t)file_browser->Buff[19]<<8 | (uint16_t)file_browser->Buff[18];	//获取图片宽度信息
+	image_height = (uint16_t)file_browser->Buff[23]<<8 | (uint16_t)file_browser->Buff[22];	//获取图片高度信息
 
 	if(image_width < LCD_W)				//要显示的图片宽度小于LCD宽度，则居中显示
 	{
@@ -85,19 +97,19 @@ void show_bmp_center(char* path)
 		offset_height = (image_height - LCD_H)/2;
 	}
 
-	temp = 54 + (image_width*3+image_width%4)*(image_height-1) - image_width*3*offset_height + 3*offset_width;	//计算将要显示的第一行第一个像素的位置
+	temp = 54 + (image_width*3+image_width%4)*(image_height-offset_height-1) + 3*offset_width;	//计算将要显示的第一行第一个像素的位置
 	fseek (f, temp, SEEK_SET);
 
 	Address_set(x1, y1, x2-1, y2-1);	//设置快速填充窗口
 	for(y=y1;y<y2;y++)
 	{
-		fread((char *)Buff, display_width*3, 1, f);
-		for(x=0;x<image_width;x++)
+		fread((char *)file_browser->Buff, display_width*3, 1, f);
+		for(x=0;x<display_width;x++)
 		{
-			buff_to_lcd[x*2] = (Buff[x*3+2] & 0xF8) | (Buff[x*3+1]>>5);
-			buff_to_lcd[x*2+1] = ((Buff[x*3+1] & 0x1C)<<3) | (Buff[x*3]>>3);
+			file_browser->buff_to_lcd[x*2] = (file_browser->Buff[x*3+2] & 0xF8) | (file_browser->Buff[x*3+1]>>5);
+			file_browser->buff_to_lcd[x*2+1] = ((file_browser->Buff[x*3+1] & 0x1C)<<3) | (file_browser->Buff[x*3]>>3);
 		}
-		lcd_write_data(buff_to_lcd, image_width*2);
+		lcd_write_data(file_browser->buff_to_lcd, display_width*2);
 
 		temp = temp - 3*image_width - image_width%4;
 		fseek (f, temp, SEEK_SET);
@@ -110,14 +122,14 @@ void show_bmp_center(char* path)
 void show_bmp_info(char* path)
 {
 	uint16_t image_width = 0, image_height = 0;
-	uint8_t line[64];
+	uint8_t line[54];
 
 	FILE *f = fopen(path, "r");
 	if (f == NULL) {
-		ESP_LOGE(TAG, "Failed to open file for reading");
+		ESP_LOGE(TAG, "2.Failed to open file for reading");
 		return;
 	}
-	fgets((char *)line, sizeof(line), f);
+	fread((char *)line, 54, 1, f);
 	fclose(f);
 
 	image_width = (uint16_t)line[19]<<8 | (uint16_t)line[18];	//获取图片宽度信息
@@ -133,7 +145,7 @@ void scan_files(char* path)
 	struct dirent * ptr;
 	dir = opendir(path);
 
-	memset(file_path, 0, 50);
+	memset(file_browser->file_path, 0, FILE_PATH_LEN);
 	while((ptr = readdir(dir)) != NULL)
 	{
 		printf("d_name : %s ", ptr->d_name);
@@ -142,9 +154,14 @@ void scan_files(char* path)
 		printf("dd_vfs_idx: %d ", dir->dd_vfs_idx);
 		printf("dd_rsv: %d ", dir->dd_rsv);
 
-		sprintf(file_path, "%.*s%.*s%.*s", strlen(path),path,strlen("/"), "/", strlen(ptr->d_name), ptr->d_name);
-		show_bmp_info(file_path);
-		display_flag = 1;
+		sprintf(file_browser->file_path,
+				"%.*s%.*s%.*s",
+				strlen(path),
+				path,strlen("/"), "/",
+				strlen(ptr->d_name),
+				ptr->d_name);
+		show_bmp_info(file_browser->file_path);
+		file_browser->display_flag = 1;
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
 	closedir(dir);
@@ -153,23 +170,21 @@ void scan_files(char* path)
 
 void sd_card_task_mem_free(void)
 {
-    // All done, unmount partition and disable SDMMC or SPI peripheral
-    esp_vfs_fat_sdmmc_unmount();
-    ESP_LOGI(TAG, "Card unmounted");
+	// All done, unmount partition and disable SDMMC or SPI peripheral
+	esp_vfs_fat_sdmmc_unmount();
+	ESP_LOGI(TAG, "Card unmounted");
 
-	free(all_file_name);
-	free(file_path);
-	free(Buff);
-	free(buff_to_lcd);
+	free(file_browser);
+	free(button_evt);
 }
 
 
 void sd_card_info_display(void)
 {
-	if(display_flag)
+	if(file_browser->display_flag == 1)
 	{
-		show_bmp_center(file_path);
-		display_flag = 0;
+		show_bmp_center(file_browser->file_path, 0, 0);
+		file_browser->display_flag = 0;
 	}
 }
 
@@ -181,12 +196,12 @@ void sd_card_task(void *pvParameter)
     sdmmc_host_t host = SDMMC_HOST_DEFAULT();
     sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
 
-    all_file_name = (char *)malloc(FILE_NUM*FILE_NAME_LEN);
-    file_path = (char *)malloc(FILE_PATH_LEN);
-    Buff = (uint8_t *)malloc(FILE_BUFF_LEN);
-    buff_to_lcd = (uint8_t *)malloc(LCD_BUFF_LEN);
+    file_browser = (file_browser_s *)malloc(sizeof(file_browser_s));
+    button_evt = (uint8_t *)malloc(BUTTON_NUM);
 
-    gpio_set_pull_mode(15, GPIO_PULLUP_ONLY);   // CMD, needed in 4- and 1- line modes
+	memset(file_browser, 0, sizeof(file_browser_s));
+
+	gpio_set_pull_mode(15, GPIO_PULLUP_ONLY);   // CMD, needed in 4- and 1- line modes
     gpio_set_pull_mode(2, GPIO_PULLUP_ONLY);    // D0, needed in 4- and 1-line modes
     gpio_set_pull_mode(4, GPIO_PULLUP_ONLY);    // D1, needed in 4-line mode only
     gpio_set_pull_mode(12, GPIO_PULLUP_ONLY);   // D2, needed in 4-line mode only
@@ -223,7 +238,21 @@ void sd_card_task(void *pvParameter)
 
     while(1)
     {
-    	vTaskDelay(20/portTICK_PERIOD_MS);
+		if(xQueueReceive(button_evt_queue, button_evt, 50/portTICK_PERIOD_MS) == pdTRUE)
+		{
+			if(button_evt[BUTTON_UP] == BUTTON_EVT_PRESSED_UP)
+			{
+			}
+
+			if(button_evt[BUTTON_DOWN] == BUTTON_EVT_PRESSED_UP)
+			{
+			}
+
+			if(button_evt[BUTTON_MIDDLE] == BUTTON_EVT_PRESSED_UP)
+			{
+
+			}
+		}
     }
 }
 
